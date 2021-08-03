a1853cc8e1469840e2ea102fc572933162afdbd8
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2021-08-03 15:19:29 +0300

    Progress

--- src/app/components/placemark-list/placemark-list.component.ts
+++ src/app/components/placemark-list/placemark-list.component.ts
@@ -2,8 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {PlacemarkService} from "../../service/placemark.service";
 import {PlacemarkModel} from "../../models/placemark.model";
 import {YaEvent} from "angular8-yandex-maps";
-import {from} from "rxjs";
-import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-placemark-list',
@@ -28,32 +26,26 @@ export class PlacemarkListComponent implements OnInit {
     const coords = event.event.get('coords');
     let latitude: number = coords[0].toPrecision(6);
     let longitude: number = coords[1].toPrecision(6);
-    let colorCode = Math.round(Math.random()*0xFFF).toString(16);
-    let color = colorCode.toString();
     let title = this.URL_FETCH + this.index.toString();
     const mPlacemark = new PlacemarkModel(title, latitude, longitude);
     const header = new Headers();
     header.append("Content-Type", "application/json");
-    const response = fetch(this.URL_FETCH, {
+    const response = await fetch(this.URL_FETCH, {
       method: "POST",
       headers: header,
       body: JSON.stringify(mPlacemark),
       cache: "no-cache"
-    })
-    .then(r => r.json()).
-    then((m_data) => {
-      console.log(m_data);
-    }).catch(reason => {console.error(reason)});
+    });
+    let parsed = response.json();
+    console.log(parsed);
     this.index++;
   }
 
   removePlacemark(event: YaEvent) {
     let placemark = event.target as ymaps.Placemark;
-    let title = placemark.properties.get('iconContent');
-    console.log("title = ", title);
-    //let res = this.service.getIdByTitle(title);
-    this.service.findByTitle(title).subscribe(item =>
-    fetch(title as unknown as string,
+    let title = placemark.properties.get('iconContent') as unknown as string;
+    this.service.findByTitle(title).subscribe(() =>
+    fetch(title,
       {
         method: 'DELETE',
         cache: "no-cache"
